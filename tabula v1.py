95b733d37dcbabd056808f3951f79d175500c538
Grandys <61079545+grandyd@users.noreply.github.com>
2020-03-09 12:37:16 +0300

    Update tabula v1.py

--- tabula v1.py
+++ tabula v1.py
@@ -17,7 +17,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 loglist = []
-logdic = {}
 
 
 def mylogs(func):
@@ -61,22 +60,24 @@ def myerrors(func):
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-@mylogs
-@myerrors
-def chat(update: Update, context: CallbackContext):
-    """Anonymous chat"""
-    global numberofusers
-    print(sad)
-    number = numberofusers
-    update.message.reply_text("Вы зашли в анонимный чат")
-    update.message.reply_text(f"В данный момент есть {number} человек готовых с вами общаться")
 
-    update.message.reply_text('Вы хотите начать общение?(да/нет)')
-
-    if update.message.text == ('да'):
-        update.message.reply_text(update.message.text)
-        number += 1
-        numberofusers = number
+"""В разработке"""
+# @mylogs
+# @myerrors
+# def chat(update: Update, context: CallbackContext):
+#     """Anonymous chat"""
+#     global numberofusers
+#     print(sad)
+#     number = numberofusers
+#     update.message.reply_text("Вы зашли в анонимный чат")
+#     update.message.reply_text(f"В данный момент есть {number} человек готовых с вами общаться")
+#
+#     update.message.reply_text('Вы хотите начать общение?(да/нет)')
+#
+#     if update.message.text == ('да'):
+#         update.message.reply_text(update.message.text)
+#         number += 1
+#         numberofusers = number
 
 
 @mylogs
@@ -89,6 +90,8 @@ def start(update: Update, context: CallbackContext):
 def history(update: Update, context: CallbackContext):
     """Send a message whe the command /history is issued."""
     bot_logs = "C:\\Users\\Георгий\\Documents\\GitHub\\course_chat_bot\\mylogs\\" + txt_name + ".txt"
+    bot_history=open("myhistory\\"+txt_name+".txt","w")
+    bot_history.close()
     line_counter=len(open(bot_logs).readlines())
     line_counter -= 1
     n = -1 - line_counter
@@ -105,33 +108,40 @@ def history(update: Update, context: CallbackContext):
         update.message.reply_text(f'Ваши последние 5 сообщений')
     with open(bot_logs, 'r') as input_file:
         lines_cache = islice(input_file, line_counter - n, line_counter)
-
+        n=1
         for curent_line in lines_cache:
             dic = eval(curent_line[0:-1])
             vivod = str(n) + '. ' + dic['message']
-            n -= 1
+            n += 1
             update.message.reply_text(vivod)
+            with open("myhistory\\"+txt_name+".txt","a") as input_file:
+                input_file.write('User: '+txt_name+"\n")
+                input_file.write('Function: '+dic["function"]+"\n")
+                input_file.write("Message: "+dic['message']+"\n\n")
+
+
 
 
 def remove(update: Update, context: CallbackContext):
     """clear logs"""
     bot_logs = "C:\\Users\\Георгий\\Documents\\GitHub\\course_chat_bot\\mylogs\\" + txt_name + ".txt"
     os.remove(bot_logs)
+    update.message.reply_text('Ваши сообщения удалены')
 
 
-@mylogs
-def last_message(update: Update, context: CallbackContext):
-    """Send a last message when the command /last_message is issued"""
-    i = 2
-    while update.effective_user.first_name != loglist[-i]['user']:
-        i += 1
-    update.message.reply_text(f'Твоё последнее сообщение было:"{loglist[-i]["message"]}"')
 
 
 @mylogs
 def chat_help(update: Update, context: CallbackContext):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Введи команду /start для начала. ')
+    update.message.reply_text('Список команд доступных для вас:')
+    update.message.reply_text('1. /start-Начало работы с ботом')
+    update.message.reply_text('2. /history-Вывод ваших последний сообщений')
+    update.message.reply_text('3. /remove-Отчистка ваших сообщение для бота')
+    update.message.reply_text('4. /chat-Начало анонимной переписки(не работает :(')
+
+
+
 
 
 @mylogs
@@ -161,8 +171,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', chat_help))
     updater.dispatcher.add_handler(CommandHandler('history', history))
-    updater.dispatcher.add_handler(CommandHandler('last_message', last_message))
-    updater.dispatcher.add_handler(CommandHandler('chat', chat))
+    # updater.dispatcher.add_handler(CommandHandler('chat', chat))
     updater.dispatcher.add_handler(CommandHandler('remove', remove))
 
     # on noncommand i.e message - echo the message on Telegram
