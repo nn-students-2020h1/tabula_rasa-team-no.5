372e10f9a71365b2ed9e3260f66f019da06896cf
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-04-14 22:18:43 +0300

    added files for homework on unittests
    class Rectangle and all the tests on it

---
+++ class_homework_1504.py
@@ -0,0 +1,46 @@
+import warnings
+class Rectangle:
+    def __init__(self, top_left_x, top_left_y, length, height):
+        self.abs = top_left_x
+        self.ord = top_left_y
+        self.l = length
+        self.h = height
+
+    def set_new_length(self, length_n):
+        if length_n < 0:
+            warnings.warn('One of the parameters is below zero', UserWarning)
+        elif length_n == 0:
+            raise ValueError('One of the parameters is zero')
+        else: self.l = length_n
+
+    def get_square(self):
+        return self.l * self.h
+
+    def is_square(self):
+        if self.l == self.h:
+            return True
+        else:
+            return None
+
+
+    def horizontal(self):
+        if self.l > self.h:
+            return True
+        else: return False
+
+
+    def in_start(self):
+        zeros = []
+        if self.abs == 0:
+            zeros.append('start_x')
+        if self.ord - self.h == 0:
+            zeros.append('start_y')
+        return zeros
+
+    def dot_inside(self, coord_x, coord_y):
+        if coord_x > self.abs and coord_x < self.abs + self.l:
+            if coord_y < self.ord and coord_y > self.ord - self.h:
+                return 'Yes'
+            else: return 'No'
+        else: return 'No'
+
---
+++ tests_for_homework.py
@@ -0,0 +1,71 @@
+import unittest
+from class_homework_1504 import Rectangle
+
+class RectangleTest(unittest.TestCase):
+    def setUp(self):
+        self.rectangle = Rectangle(5, 8, 12, 8)
+
+    def tearDown(self):
+        self.rectangle.zeros = []
+
+    def test_equal(self):
+        square = self.rectangle.get_square()
+        self.assertEqual(square, 96)
+
+    def test_notqeual(self):
+        square = self.rectangle.get_square()
+        self.assertNotEqual(square, 42)
+
+    def test_True(self):
+        sq = self.rectangle.horizontal()
+        self.assertTrue(sq)
+
+    def test_False(self):
+        sq = self.rectangle.is_square()
+        self.assertFalse(sq)
+
+    def test_is(self):
+        dot_1 = self.rectangle.dot_inside(15, 6)
+        dot_2 = self.rectangle.dot_inside(7, 6)
+        self.assertIs(dot_1, dot_2)
+
+    def test_is_not(self):
+        dot_1 = self.rectangle.dot_inside(12, 5)
+        dot_2 = self.rectangle.dot_inside(3, 9)
+        self.assertIsNot(dot_1, dot_2)
+
+    def test_in(self):
+        zeros = self.rectangle.in_start()
+        self.assertIn('start_y', zeros)
+
+    def test_not_in(self):
+        zeros = self.rectangle.in_start()
+        self.assertNotIn('start_x', zeros)
+
+    def test_none(self):
+        sq = self.rectangle.is_square()
+        self.assertIsNone(sq)
+
+    def test_not_none(self):
+        dot = self.rectangle.dot_inside(1, 1)
+        self.assertIsNotNone(dot)
+
+    def test_is_instance(self):
+        dot = self.rectangle.dot_inside(7, 6)
+        self.assertIsInstance(dot, str)
+
+    def test_is_not_instance(self):
+        dot = self.rectangle.dot_inside(15, 3)
+        self.assertIsInstance(dot, str)
+
+    def test_warn(self):
+        with self.assertWarns(UserWarning):
+            self.rectangle.set_new_length(-5)
+
+    def test_error(self):
+        with self.assertRaises(ValueError):
+            self.rectangle.set_new_length(0)
+
+
+if __name__ == '__main__':
+    unittest.main()
