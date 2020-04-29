c384820435173b07f75b610c7915731979fae6ee
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-04-29 11:13:39 +0300

    временное удаление тестов

--- tests/test_csv_class.py
+++ tests/test_csv_class.py
@@ -68,57 +68,5 @@ class TestAnalyser(unittest.TestCase):
             {'Country': 'USA', 'Parametr': 5},
             {'Country': 'Russia', 'Parametr': 2}])
 
-
-class TestCorona(unittest.TestCase):
-    def setUp(self):
-        self.context = mock.MagicMock()
-        self.update = mock.MagicMock()
-        self.update.message = 'bla-bla'
-        self.CallbackContext = ''
-        with patch('tabula_rasa_main.corona.find_one') as mock_get:
-            mock_get.return_value = reader
-            self.analyser = tabula_rasa_main.AnalyseCSV()
-        self.analyser.yesterday = reader_yesterday
-
-    @patch('tabula_rasa_main.TODAY', 'some date')
-    def test_corono_stats(self):
-        self.update = mock.MagicMock(spec=['message'])
-        with patch.object(tabula_rasa_main.AnalyseCSV, 'compare_days') as mock_dynamics:
-            mock_dynamics.return_value = [
-                {'Country': 'Russia', 'Parametr': 5},
-                {'Country': 'USA', 'Parametr': 7},
-                {'Country': 'Germany', 'Parametr': 15}]
-            text = tabula_rasa_main.corono_stats(self.update, self.CallbackContext)
-        answer = '5 провинций с наибольшим числом заражённых (some date)\nСтрана: Germany | Число зараженных: 15\nСтрана: USA | Число зараженных: 7\nСтрана: Russia | Число зараженных: 5\n'
-        self.assertEqual(text, answer)
-
-    @patch('tabula_rasa_main.TODAY', 'some date')
-    def test_corona_stats_dynamic(self):
-        self.update = mock.MagicMock(spec=['message'])
-        with patch.object(tabula_rasa_main.AnalyseCSV, 'compare_days') as mock_dynamics:
-            mock_dynamics.return_value = [
-                {'Country': 'Germany', 'Parametr': 14},
-                {'Country': 'USA', 'Parametr': 5},
-                {'Country': 'Russia', 'Parametr': 2}]
-            text = tabula_rasa_main.corona_stats_dynamic(self.update, self.CallbackContext)
-        self.assertEqual(text,
-                         '5 провинций с наибольшим числом новых заражённых (some date)\n'
-                         'Страна: Germany | Количество новых зараженных 14 \n'
-                         'Страна: USA | Количество новых зараженных 5 \n'
-                         'Страна: Russia | Количество новых зараженных 2 \n')
-
-    @patch('tabula_rasa_main.TODAY', 'some date')
-    def test_corona_world_dynamic(self):
-        self.update = mock.MagicMock(spec=['message'])
-        with patch.object(tabula_rasa_main.AnalyseCSV, 'compare_days') as mock_dynamics:
-            mock_dynamics.return_value = [
-                {'Country': 'Germany', 'Parametr': 14},
-                {'Country': 'USA', 'Parametr': 5},
-                {'Country': 'Russia', 'Parametr': 2}]
-            text = tabula_rasa_main.corona_world_dynamic(self.update, self.CallbackContext)
-        self.assertEqual(text,
-                         'Мировая статистика за прошедшие сутки:\nНовых заражённых: 21\nУмерло: 21\nВыздоровело: 21\n')
-
-
 if __name__ == '__main__':
     unittest.main()
