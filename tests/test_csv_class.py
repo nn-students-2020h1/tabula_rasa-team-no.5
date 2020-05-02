ccbd18ea38b26ccfeb28aa82b84d27e3c932d388
Карабасова Юлия Алексеевна <61205728+juliakarabasova@users.noreply.github.com>
2020-05-02 17:56:41 +0300

    return deleted tests

--- tests/test_csv_class.py
+++ tests/test_csv_class.py
@@ -16,17 +16,13 @@ reader_yesterday = [
     {'Country_Region': 'USA', 'Active': 2, 'Deaths': 0, 'Recovered': 47},
     {'Country_Region': 'Germany', 'Active': 1, 'Deaths': 0, 'Recovered': 13}]
 
-
-def use_test():
-    yield '01-01-97', 'some_site'
-    yield '31-12-96', 'some_site'
+client = mongomock.MongoClient('127.0.0.1', 27017)
+db = client['somedb']
+db.create_collection('log')
 
 
 class TestAnalyser(unittest.TestCase):
     def setUp(self):
-        self.client = mongomock.MongoClient()
-        self.db = self.client['somedb']
-        self.collection = self.db.logs
         self.update = mock.MagicMock()
         self.update.message.text = 'bla-bla'
         self.update.effective_user.first_name = 'your name'
@@ -37,18 +33,19 @@ class TestAnalyser(unittest.TestCase):
         self.analyser.yesterday = reader_yesterday
 
     def tearDown(self) -> None:
-        self.db.logging.delete_many({})
-        global loglist
-        loglist = []
+        db.log.delete_many({})
 
+    @patch('tabula_rasa_main.collection', db.log)
     def test_count_all(self):
         number = self.analyser.count_all('Active')
         self.assertEqual(number, 27.0)
 
+    @patch('tabula_rasa_main.collection', db.log)
     def test_top_n(self):
         top = self.analyser.top_n('Deaths', 2)
         self.assertEqual(top, [9, 6])
 
+    @patch('tabula_rasa_main.collection', db.log)
     def test_top_covid(self):
         list_dict = self.analyser.top_covid()
         self.assertEqual(list_dict, [
@@ -56,17 +53,74 @@ class TestAnalyser(unittest.TestCase):
             {'Country': 'USA', 'Parametr': 7},
             {'Country': 'Russia', 'Parametr': 5}])
 
+    @patch('tabula_rasa_main.collection', db.log)
     def test_compare_days_not_compare(self):
         self.assertEqual(self.analyser.compare_days('Active'), [
             {'Country': 'Russia', 'Parametr': 5},
             {'Country': 'USA', 'Parametr': 7},
             {'Country': 'Germany', 'Parametr': 15}])
 
+    @patch('tabula_rasa_main.collection', db.log)
     def test_compare_days(self):
         self.assertEqual(self.analyser.compare_days('Active', compare=True), [
             {'Country': 'Germany', 'Parametr': 14},
             {'Country': 'USA', 'Parametr': 5},
             {'Country': 'Russia', 'Parametr': 2}])
+
+
+class TestCorona(unittest.TestCase):
+    def setUp(self):
+        self.context = mock.MagicMock()
+        self.update = mock.MagicMock()
+        self.update.message = 'bla-bla'
+        self.CallbackContext = mock.MagicMock()
+        with patch('tabula_rasa_main.corona.find_one') as mock_get:
+            mock_get.return_value = reader
+            self.analyser = tabula_rasa_main.AnalyseCSV()
+        self.analyser.yesterday = reader_yesterday
+
+    @patch('tabula_rasa_main.collection', db.log)
+    @patch('tabula_rasa_main.TODAY', 'some date')
+    def test_corono_stats(self):
+        self.update = mock.MagicMock(spec=['message'])
+        with patch.object(tabula_rasa_main.AnalyseCSV, 'compare_days') as mock_dynamics:
+            mock_dynamics.return_value = [
+                {'Country': 'Russia', 'Parametr': 5},
+                {'Country': 'USA', 'Parametr': 7},
+                {'Country': 'Germany', 'Parametr': 15}]
+            text = tabula_rasa_main.corono_stats(self.update, self.CallbackContext)
+        answer = '5 провинций с наибольшим числом заражённых (some date)\nСтрана: Germany | Число зараженных: 15\nСтрана: USA | Число зараженных: 7\nСтрана: Russia | Число зараженных: 5\n'
+        self.assertEqual(text, answer)
+
+    @patch('tabula_rasa_main.collection', db.log)
+    @patch('tabula_rasa_main.TODAY', 'some date')
+    def test_corona_stats_dynamic(self):
+        self.update = mock.MagicMock(spec=['message'])
+        with patch.object(tabula_rasa_main.AnalyseCSV, 'compare_days') as mock_dynamics:
+            mock_dynamics.return_value = [
+                {'Country': 'Germany', 'Parametr': 14},
+                {'Country': 'USA', 'Parametr': 5},
+                {'Country': 'Russia', 'Parametr': 2}]
+            text = tabula_rasa_main.corona_stats_dynamic(self.update, self.CallbackContext)
+        self.assertEqual(text,
+                         '5 провинций с наибольшим числом новых заражённых (some date)\n'
+                         'Страна: Germany | Количество новых зараженных 14 \n'
+                         'Страна: USA | Количество новых зараженных 5 \n'
+                         'Страна: Russia | Количество новых зараженных 2 \n')
+
+    @patch('tabula_rasa_main.collection', db.log)
+    @patch('tabula_rasa_main.TODAY', 'some date')
+    def test_corona_world_dynamic(self):
+        self.update = mock.MagicMock(spec=['message'])
+        with patch.object(tabula_rasa_main.AnalyseCSV, 'compare_days') as mock_dynamics:
+            mock_dynamics.return_value = [
+                {'Country': 'Germany', 'Parametr': 14},
+                {'Country': 'USA', 'Parametr': 5},
+                {'Country': 'Russia', 'Parametr': 2}]
+            text = tabula_rasa_main.corona_world_dynamic(self.update, self.CallbackContext)
+        self.assertEqual(text,
+                         'Мировая статистика за прошедшие сутки:\nНовых заражённых: 21\nУмерло: 21\nВыздоровело: 21\n')
+
 
 if __name__ == '__main__':
     unittest.main()
